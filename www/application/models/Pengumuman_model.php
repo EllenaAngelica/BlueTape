683a2250c88dd679fe6f2479c7faa128c7547376
EllenaAngelica <ellenaangelica98@gmail.com>
2018-10-01 18:07:14 +0700

    Fixing Pengumuman Model

--- www/application/models/Pengumuman_model.php
+++ www/application/models/Pengumuman_model.php
@@ -25,35 +25,19 @@ class Pengumuman_model extends CI_Model {
 		$emails = imap_search($inbox,'ALL');
 
 		if($emails) {
-			
-			/* begin output var */
-			$output = '';
-			
-			/* put the newest emails on top */
 			rsort($emails);
-			
-			/* for every email... */
 			foreach($emails as $email_number) {
-				
-				/* get information specific to this email */
 				$overview = imap_fetch_overview($inbox,$email_number,0);
 				$message = imap_fetchbody($inbox,$email_number,2);
 				
-				/* output the email header information */
-				$output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
-				$output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
-				$output.= '<span class="from">'.$overview[0]->from.'</span>';
-				$output.= '<span class="date">on '.$overview[0]->date.'</span>';
-				$output.= '</div>';
-				
-				/* output the email body */
-				$output.= '<div class="body">'.$message.'</div>';
+				echo "Status : " . ($overview[0]->seen ? 'read' : 'unread') . "\n";
+				echo "Subject : " . $overview[0]->subject . "\n";
+				echo "From : " . $overview[0]->from . "\n";
+				echo "Date : " . $overview[0]->date . "\n";
+				echo "Body : " . $message . "\n";
 			}
-			
-			echo $output;
 		} 
 
-		/* close the connection */
 		imap_close($inbox);
 	}
 }
