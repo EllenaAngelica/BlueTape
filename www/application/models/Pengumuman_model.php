15d8e44ec643e7c5246759c8d481ca11db8337b3
EllenaAngelica <ellenaangelica98@gmail.com>
2018-10-11 21:01:44 +0700

    Fixing bug

--- www/application/models/Pengumuman_model.php
+++ www/application/models/Pengumuman_model.php
@@ -63,8 +63,8 @@ class Pengumuman_model extends CI_Model {
 		$email_id = null;
 		
 		$this->db->select('id');
-		$this->db->get_where->('Pengirim_Terverifikasi', ['email'=>$new_email['email_from']],1);
-		if(query->num_rows() = 1){
+		$query = $this->db->get_where('Pengirim_Terverifikasi', ['email'=>$new_email['email_from']],1);
+		if($query->num_rows() == 1){
 			$email_id = $query->result()->id;
 		}
 		
