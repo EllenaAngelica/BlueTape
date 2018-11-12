60bdc63bcb312a259429ae7265108273f1558fcd
EllenaAngelica <ellenaangelica98@gmail.com>
2018-11-12 13:15:03 +0700

    Fixing webhook

--- www/application/controllers/Line.php
+++ www/application/controllers/Line.php
@@ -4,33 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Line extends CI_Controller {
 
 	public function webhook(){
-		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('LINE_BOT_CHANNEL_TOKEN'));
-		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('LINE_BOT_CHANNEL_SECRET')]);
-		
+		try{
+			$request = json_decode(file_get_contents('php://input'), true);
 
-		$httpBody = file_get_contents('php://input');
-		/**
-		$signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : "-";
-		if (empty($signature)) {
-			return $res->withStatus(400, 'Bad Request');
-        }
-		
-		try {
-        	$events = $bot->parseEventRequest($req->getBody(), $signature[0]);
-        } catch (InvalidSignatureException $e) {
-			return $res->withStatus(400, 'Invalid signature');
-		} catch (InvalidEventRequestException $e) {
-			return $res->withStatus(400, "Invalid event request");
-        }
-		**/
-		$events = json_decode($httpBody, true);
-		
-		foreach ($events['events'] as $event) {
-			if ($event instanceof FollowEvent) {
-				$this->bot->replyText($this->followEvent->getReplyToken(), 'Got followed event');
-			}
+			http_response_code(200);
 		}
-		http_response_code(200);
-	}
+		catch(Exception $e){
+			http_response_code(500);
+		}
 }
 
--- www/application/models/Line_model.php
+++ www/application/models/Line_model.php
@@ -2,20 +2,57 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Line_model extends CI_Model {
-	public function __construct() {
-        parent::__construct();
+	private $channelAccessToken;
+	private $channelSecret;
+	private $webhookResponse;
+	private $webhookEventObject;
+	private $apiReply;
+	private $apiPush;
+	
+	public function __construct(){
+		parent::__construct();
 	
         $this->load->config('auth');
         $this->load->config('modules');
+		
+		$this->channelAccessToken = getenv('LINE_BOT_CHANNEL_TOKEN');
+		$this->channelSecret = getenv('LINE_BOT_CHANNEL_SECRET');
+		$this->apiReply = "https://api.line.me/v2/bot/message/reply";
+		$this->apiPush = "https://api.line.me/v2/bot/message/push";
+		$this->webhookResponse = file_get_contents('php://input');
+		$this->webhookEventObject = json_decode($this->webhookResponse);
+	}
+	
+	private function httpPost($api,$body){
+		$ch = curl_init($api); 
+		curl_setopt($ch, CURLOPT_POST, true); 
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body)); 
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array( 
+		'Content-Type: application/json; charser=UTF-8', 
+		'Authorization: Bearer '.$this->channelAccessToken)); 
+		$result = curl_exec($ch); 
+		curl_close($ch); 
+		return $result;
+	}
+	
+	public function push($body){
+		$api = $this->apiPush;
+		$result = $this->httpPost($api, $body);
+		return $result;
     }
 	
-	public function pushMessage($message) {
-		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('LINE_BOT_CHANNEL_TOKEN'));
-		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('LINE_BOT_CHANNEL_SECRET')]);
-
-		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-		$response = $bot->pushMessage('<to>', $textMessageBuilder);
-			
-		echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-	}
+	public function pushMessage($to, $text){
+		$body = array(
+		    'to' => $to,
+		    'messages' => [
+			array(
+			    'type' => 'text',
+			    'text' => $text
+			)
+		    ]
+		);
+		$this->push($body);
+	 }
 }
