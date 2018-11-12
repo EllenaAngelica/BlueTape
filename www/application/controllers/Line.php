6bbea9e713625e41bffbaca184c25277ec33acfe
EllenaAngelica <ellenaangelica98@gmail.com>
2018-11-12 11:35:58 +0700

    Fixing webhook

--- www/application/controllers/Line.php
+++ www/application/controllers/Line.php
@@ -3,25 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Line extends CI_Controller {
 
-	public function webhook($req, $res){
+	public function webhook(\Slim\Http\Request $req, \Slim\Http\Response $res){
 		$channelSecret = getenv('LINE_BOT_CHANNEL_SECRET');
 		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('LINE_BOT_CHANNEL_TOKEN'));
 		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 		
 		$signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
 		if (empty($signature)) {
-			http_response_code(400);
-        	//'Bad Request'
+			return $res->withStatus(400, 'Bad Request');
         }
 		
 		try {
         	$events = $bot->parseEventRequest($req->getBody(), $signature[0]);
         } catch (InvalidSignatureException $e) {
-			http_response_code(400);
-			//'Invalid signature'
+			return $res->withStatus(400, 'Invalid signature');
 		} catch (InvalidEventRequestException $e) {
-			http_response_code(400); 
-			//"Invalid event request"
+			return $res->withStatus(400, "Invalid event request");
         }
 		
 		foreach ($events as $event) {
@@ -30,6 +27,8 @@ class Line extends CI_Controller {
 			}
 		}
 		http_response_code(200);
+		$res->write('OK');
+        return $res;
 	}
 }
 
