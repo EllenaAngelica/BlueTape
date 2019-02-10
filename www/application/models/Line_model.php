383ca9b7c19ecd2f6a68a7edafd442a6c40eae4c
EllenaAngelica <ellenaangelica98@gmail.com>
2019-02-10 22:56:28 +0700

    Add and fix Line Model

--- www/application/models/Line_model.php
+++ www/application/models/Line_model.php
@@ -1,13 +1,32 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Event\AccountLinkEvent;
+use LINE\LINEBot\Event\BeaconDetectionEvent;
+use LINE\LINEBot\Event\FollowEvent;
+use LINE\LINEBot\Event\JoinEvent;
+use LINE\LINEBot\Event\LeaveEvent;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\AudioMessage;
+use LINE\LINEBot\Event\MessageEvent\ImageMessage;
+use LINE\LINEBot\Event\MessageEvent\LocationMessage;
+use LINE\LINEBot\Event\MessageEvent\StickerMessage;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Event\MessageEvent\UnknownMessage;
+use LINE\LINEBot\Event\MessageEvent\VideoMessage;
+use LINE\LINEBot\Event\PostbackEvent;
+use LINE\LINEBot\Event\UnfollowEvent;
+use LINE\LINEBot\Event\UnknownEvent;
+use LINE\LINEBot\Exception\InvalidEventRequestException;
+use LINE\LINEBot\Exception\InvalidSignatureException;
+
 class Line_model extends CI_Model {
 	private $channelAccessToken;
 	private $channelSecret;
-	private $webhookResponse;
-	private $webhookEventObject;
-	private $apiReply;
-	private $apiPush;
+	private $httpClient;
+	private $bot;
 	
 	public function __construct(){
 		parent::__construct();
@@ -17,42 +36,82 @@ class Line_model extends CI_Model {
 		
 		$this->channelAccessToken = getenv('LINE_BOT_CHANNEL_TOKEN');
 		$this->channelSecret = getenv('LINE_BOT_CHANNEL_SECRET');
-		$this->apiReply = "https://api.line.me/v2/bot/message/reply";
-		$this->apiPush = "https://api.line.me/v2/bot/message/push";
-		$this->webhookResponse = file_get_contents('php://input');
-		$this->webhookEventObject = json_decode($this->webhookResponse);
+		$this->httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->channelAccessToken);
+		$this->bot = new \LINE\LINEBot($this->httpClient, ['channelSecret' => $this->channelSecret]);
+	}
+
+	private function validateSignature($xLineSignature, $httpRequestBody){
+		$hash = hash_hmac('sha256', $httpRequestBody, $this->channelSecret, true);
+		$signature = base64_encode($hash);
+		return hash_equals($signature, $xLineSignature);
+	}
+
+	public function proceedWebhook($xLineSignature, $httpRequestBody){
+		$valid = $this->validateSignature($xLineSignature, $httpRequestBody);
+		if($valid){
+			try{
+				$events = $this->bot->parseEventRequest($httpRequestBody, $xLineSignature);
+			} catch (InvalidSignatureException $e) {
+				http_response_code(400); // Invalid signature
+			} catch (InvalidEventRequestException $e) {
+				http_response_code(400); // Invalid event request
+			}
+
+			foreach ($events as $event) {
+                if ($event instanceof MessageEvent) {
+                    if ($event instanceof TextMessage) {
+                        
+                    } elseif ($event instanceof StickerMessage) {
+                        
+                    } elseif ($event instanceof LocationMessage) {
+                        
+                    } elseif ($event instanceof ImageMessage) {
+                        
+                    } elseif ($event instanceof AudioMessage) {
+                        
+                    } elseif ($event instanceof VideoMessage) {
+                        
+                    } elseif ($event instanceof UnknownMessage) {
+                        http_response_code(400); // Invalid event type
+                    } else {
+						http_response_code(400); // Invalid event type
+                    }
+                } elseif ($event instanceof UnfollowEvent) {
+					$id = $this->bot->getUserId();
+					$this->db->delete('Line_Followers', array('userId' => $id));
+                } elseif ($event instanceof FollowEvent) {
+					$this->db->insert('Line_Followers', array(
+						'userId' => $this->bot->getUserId()
+					));
+                    $this->bot->replyText($event->getReplyToken(), 'Thank you for following me! XD');
+                } elseif ($event instanceof JoinEvent) {
+                    
+                } elseif ($event instanceof LeaveEvent) {
+                    
+                } elseif ($event instanceof PostbackEvent) {
+                    
+                } elseif ($event instanceof BeaconDetectionEvent) {
+                    
+                } elseif ($event instanceof AccountLinkEvent) {
+                    
+                } elseif ($event instanceof UnknownEvent) {
+                    http_response_code(400); // Invalid event type
+                } else {
+					http_response_code(400); // Invalid event type
+                }
+            }
+		}
 	}
 	
-	private function httpPost($api,$body){
-		$ch = curl_init($api); 
-		curl_setopt($ch, CURLOPT_POST, true); 
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body)); 
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array( 
-		'Content-Type: application/json; charser=UTF-8', 
-		'Authorization: Bearer '.$this->channelAccessToken)); 
-		$result = curl_exec($ch); 
-		curl_close($ch); 
-		return $result;
+	public function pushMessageToAllFollowers($text){
+		$tos = [];
+		$query = $this->db->get('Line_Followers');
+		foreach ($query->result() as $row){
+			$tos[] = $row->userId;
+		}
+
+        $ref = new ReflectionClass('LINE\LINEBot\MessageBuilder\TextMessageBuilder');
+        $textMessageBuilder = $ref->newInstanceArgs(array_merge([$text]));
+		$this->bot->multicast($tos, $textMessageBuilder);
 	}
-	
-	public function push($body){
-		$api = $this->apiPush;
-		$result = $this->httpPost($api, $body);
-		return $result;
-    }
-	
-	public function pushMessage($to, $text){
-		$body = array(
-		    'to' => $to,
-		    'messages' => [
-			array(
-			    'type' => 'text',
-			    'text' => $text
-			)
-		    ]
-		);
-		$this->push($body);
-	 }
 }
