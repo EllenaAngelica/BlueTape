4301816f48145d518a0a97a79cffe22b4eb90954
EllenaAngelica <ellenaangelica98@gmail.com>
2019-04-26 02:34:55 +0700

    Handle invalid email,fix fetch body&check attach

--- www/application/models/Pengumuman_model.php
+++ www/application/models/Pengumuman_model.php
@@ -23,39 +23,65 @@ class Pengumuman_model extends CI_Model {
 		if($emails) {
 			$i = 0;
 			foreach($emails as $emailNumber) {
+				$invalid = false;
 				$header = imap_headerinfo($inbox,$emailNumber);
-				$from = $header->from;
-				$bodymsg = '';
-				$attachmentExist = 'N';
-				foreach($from as $id => $object){
-					$fromaddress = $object->mailbox . "@" . $object->host;
-				}
-				
-				$structure = imap_fetchstructure($inbox, $emailNumber);
-
-				if(isset($structure->parts) && is_array($structure->parts) && isset($structure->parts[1])) {
+				$from = isset($header->from) ? $header->from : null;
+				if($from != null){
+					$fromaddress = null;
+					foreach($from as $id => $object){
+						$fromaddress = isset($object->mailbox) && isset($object->host) ? $object->mailbox . "@" . $object->host : null;
+					}
+					$bodymsg = '';
 					$attachmentExist = 'N';
-
-					if(isset($structure->parts[0]->parts)){
-						$attachmentExist = 'Y';
+					$structure = imap_fetchstructure($inbox, $emailNumber);
+					if(isset($structure->parts) && is_array($structure->parts)) {
+						if(isset($structure->parts[1])){
+							$parts1 = $structure->parts[1];
+							if(isset($parts1->disposition) && $parts1->disposition == "ATTACHMENT"){
+								$attachmentExist = 'Y';
+								$bodymsg = imap_qprint(imap_fetchbody($inbox, $emailNumber, '1.2'));
+								if (empty($bodymsg)) {
+									$bodymsg = imap_qprint(imap_fetchbody($inbox, $emailNumber, '1.1'));
+								}
+							}
+							else{
+								$bodymsg = imap_qprint(imap_fetchbody($inbox, $emailNumber, '2'));
+								if (empty($bodymsg)) {
+									$bodymsg = imap_qprint(imap_fetchbody($inbox, $emailNumber, '1'));
+								}
+							}
+						}
 					}
 
-					$bodymsg = imap_qprint(imap_fetchbody($inbox, $emailNumber, 1.2));
+					if($fromaddress != null){
+						$newEmails[$i]['emailFrom'] = $fromaddress;
+						$newEmails[$i]['from'] = isset($header->fromaddress) ? $header->fromaddress : $fromaddress;
+						if(isset($header->udate)){
+							$newEmails[$i]['date'] = date("Y-m-d H:i:s", $header->udate);
+						}
+						else{
+							$invalid = true;
+						}
+						
+						if(isset($header->subject)){
+							$newEmails[$i]['subject'] = $header->subject;
+						}
+						else{
+							$invalid = true;
+						}
 
-					if (empty($bodymsg)) {
-						$bodymsg = imap_qprint(imap_fetchbody($inbox, $emailNumber, 1));
+						$newEmails[$i]['body'] = $bodymsg;
+						$newEmails[$i]['attachmentExist'] = $attachmentExist;
 					}
 				}
-				
-				$newEmails[$i]['emailFrom'] = $fromaddress;
-				$newEmails[$i]['from'] = $header->fromaddress;
-				$newEmails[$i]['date'] = date("Y-m-d H:i:s", $header->udate);
-				$newEmails[$i]['subject'] = $header->subject;
-				$newEmails[$i]['body'] = $bodymsg;
-				$newEmails[$i]['attachmentExist'] = $attachmentExist;
-				$i++;
+				if($invalid){
+					unset($newEmails[$i]);
+				}
+				else{
+					$i++;
+				}
 			}
-		} 
+		}
 		
 		$errors = imap_errors();
 
